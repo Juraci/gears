f2c54bc8a6f7719fbcf9d206ca3dbfba1140c076
Juraci Vieira <juraci.vieira@gmail.com>
2020-05-08 11:28:15 -0300

    refactoring spec dependencies

--- spec/gears_spec.rb
+++ spec/gears_spec.rb
@@ -1,9 +1,22 @@
 require "spec_helper"
 
 RSpec.describe Gear do
-  describe "#ratio" do
-    it "returns the ratio between chainring and cog" do
-      expect(Gear.new(52, 11).ratio).to eq 4.7272727272727275
+  let(:chainring) { 52 }
+  let(:cog)       { 11 }
+  let(:rim)       { 26 }
+  let(:tire)      { 1.5 }
+
+  subject { described_class.new(chainring, cog, rim, tire) }
+
+  describe '#ratio' do
+    it 'returns the ratio between chainring and cog' do
+      expect(subject.ratio).to eq 4.7272727272727275
+    end
+  end
+
+  describe '#gear_inches' do
+    it 'returns the result based on the wheel diameter times the gear ratio' do
+      expect(subject.gear_inches).to eq 137.0909090909091
     end
   end
 end
